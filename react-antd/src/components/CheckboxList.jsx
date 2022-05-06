19721e52d61db4abf9fea548e447ffbb6eb80304
coulsonzero <coulson2021@163.com>
2022-05-06 12:06:28 +0800

    update

--- react-antd/src/components/CheckboxList.jsx
+++ react-antd/src/components/CheckboxList.jsx
@@ -11,18 +11,18 @@ function CheckboxList() {
 				{
 					label: "1st menu item",
 					key: "1",
-					icon: <UserOutlined />,
+					icon: <UserOutlined />
 				},
 				{
 					label: "2nd menu item",
 					key: "2",
-					icon: <UserOutlined />,
+					icon: <UserOutlined />
 				},
 				{
 					label: "3rd menu item",
 					key: "3",
-					icon: <UserOutlined />,
-				},
+					icon: <UserOutlined />
+				}
 			]}
 		/>
 	)
